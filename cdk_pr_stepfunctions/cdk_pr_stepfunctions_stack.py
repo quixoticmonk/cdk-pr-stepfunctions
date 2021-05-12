18ff765ff548705dc237be8540f3298256d20d75
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-05-11 22:27:23 -0400

    Initial commit

--- cdk_pr_stepfunctions/cdk_pr_stepfunctions_stack.py
+++ cdk_pr_stepfunctions/cdk_pr_stepfunctions_stack.py
@@ -1,15 +1,69 @@
-from aws_cdk import core as cdk
+from aws_cdk import (
+    aws_stepfunctions as _step_fn,
+    aws_stepfunctions_tasks as _tasks,
+    aws_codebuild as _codebuild,
+    aws_codecommit as codecommit
+)
+from aws_cdk.core import (
+    Stack,
+    Construct,
+    Duration
+)
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
+class CdkPrStepfunctionsStack(Stack):
 
-class CdkPrStepfunctionsStack(cdk.Stack):
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        repo = codecommit.Repository(
+            self,
+            "repo",
+            repository_name="demorepo",
+            description="Repo to test PR with stepfunctions"
+        )
+
+        proj1 = self.new_build_project(repo, "pr_specs/buildspec.yaml", "proj1")
+        proj2 = self.new_build_project(repo, "pr_specs/buildspec.yaml", "proj2")
+
+        proj1_tasks = self.new_codebuild_task(proj1)
+        proj2_tasks = self.new_codebuild_task(proj2)
+
+        definition = proj1_tasks.next(proj2_tasks)
+
+        _fn = _step_fn.StateMachine(
+            self,
+            "statemachine",
+            definition=definition,
+            state_machine_name="statemachine",
+        )
+
+    def new_codebuild_task(self, project:_codebuild.Project) -> _tasks.CodeBuildStartBuild:
+        return _tasks.CodeBuildStartBuild(
+            self,
+            f"{project}task",
+            project=project,
+            timeout=Duration.minutes(10)
+        )
+
+    def new_build_project(self, repo: codecommit.Repository, buildspec_path: str, proj_name: str) -> _codebuild.Project:
+        return _codebuild.Project(
+            self,
+            proj_name,
+            badge=True,
+            source=_codebuild.Source.code_commit(
+                repository=repo
+            ),
+            description=f"Build project for {proj_name}",
+            environment=_codebuild.BuildEnvironment(
+                build_image=_codebuild.LinuxBuildImage.AMAZON_LINUX_2,
+                compute_type=_codebuild.ComputeType.LARGE,
+                privileged=True
+            ),
+            project_name=proj_name,
+            build_spec=_codebuild.BuildSpec.from_source_filename(
+                filename=buildspec_path
+            ),
+            timeout=Duration.minutes(10),
+
+        )
